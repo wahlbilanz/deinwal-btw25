11043832a521e37e000d361bd263b7aaa143e4a7
Martin Scharm <martin@binfalse.de>
2025-01-29 00:38:15 +0100

    added logos zur auswerungsseite

--- src/app/components/auswertung-accordion-item/auswertung-accordion-item.component.ts
+++ src/app/components/auswertung-accordion-item/auswertung-accordion-item.component.ts
@@ -1,16 +1,35 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { DeinwalFrage } from '../../interfaces/data.interface';
 import { AuswertungAccordionDetailsComponent } from '../auswertung-accordion-details/auswertung-accordion-details.component';
 import { AngularLineawesomeModule } from 'angular-line-awesome';
 import { AntwortIconComponent } from '../antwort-icon/antwort-icon.component';
-import { Antwort } from '../../interfaces/antworten.interface';
+import { Antwort, Uebereinstimmung } from '../../interfaces/antworten.interface';
+import { FraktionBadgeComponent } from '../fraktion-badge/fraktion-badge.component';
+import { KeyValuePipe } from '@angular/common';
+import { generateMap } from '../../functions/data-massage.function';
+import { AGREEMENT } from '../../enums/agreement.enum';
 
 @Component({
   selector: 'wal-auswertung-accordion-item',
-  imports: [AuswertungAccordionDetailsComponent, AngularLineawesomeModule, AntwortIconComponent],
+  imports: [
+    AuswertungAccordionDetailsComponent,
+    AngularLineawesomeModule,
+    AntwortIconComponent,
+    FraktionBadgeComponent,
+    KeyValuePipe,
+  ],
   templateUrl: './auswertung-accordion-item.component.html',
 })
 export class AuswertungAccordionItemComponent {
   public abstimmung = input.required<DeinwalFrage | undefined>();
   public antwort = input.required<Antwort>();
+  public uebereinstimmungen = computed<{ [p: string]: Uebereinstimmung }>(() => {
+    const a = this.antwort();
+    if (!a?.uebereinstimmungen) {
+      return {};
+    }
+    return generateMap(
+      Object.entries(a?.uebereinstimmungen).filter(x => x[1].agreement === AGREEMENT.AGREE),
+    );
+  });
 }
